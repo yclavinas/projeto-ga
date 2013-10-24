1ed4e1a71e56b13225be6bdeb3a6ddd993c7a3e0
yurilavinas <yclavinas@gmail.com>
2013-10-24 16:39:12 -0200

    arrumado, mas ainda com alto valor negativo

--- log-likelihood.py
+++ log-likelihood.py
@@ -1,6 +1,7 @@
 #nao tratei lamba = 0, por no momento nao termos o caso -> implementacoes futuras
 import math
 from data_loglike import *
+from fatorial import *
 
 #entra com tamanho do vetor de obs, tamanho do agrupamento e nome do arq e seu mode de abertura
 observations, expetations, total_obs, quant_por_grupo =coletar_data(77398, 7,"jma_cat_2000_2012_Mth2.5_formatted.dat", 'r')
@@ -15,6 +16,6 @@ log_likelihood = [None]*7
 
 for i in range(7):
 	k = i % 7
-	log_likelihood[k] = (-expetations[k]) + (quant_por_grupo[i]*math.log10(expetations[k])) - (math.log10(quant_por_grupo[i]))
+	log_likelihood[k] = -expetations[k] + (quant_por_grupo[i]*math.log10(expetations[k])) - (math.log10(fat(quant_por_grupo[i])))
 
 print(log_likelihood)
