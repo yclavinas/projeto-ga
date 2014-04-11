209a80d775dd58eea22834318d7a41a61a6e961e
lara04 <lara04@lara04pc.(none)>
2014-04-10 21:12:54 -0300

    -3 maq

--- testes_cx_deap/100_exec.py
+++ testes_cx_deap/100_exec.py
@@ -54,30 +54,56 @@ for w in range(8):
 	# 	    os.waitpid(childBlend, 0)
 
 
-	for i in range(2):
-		NUM_PROCESSES = 25
-	 	def timeConsumingFunction():
-	 	    x = 1
-	 	    for n in xrange(1000000):
-	 	        x += 1
+	# for i in range(2):
+	# 	NUM_PROCESSES = 25
+	#  	def timeConsumingFunction():
+	#  	    x = 1
+	#  	    for n in xrange(1000000):
+	#  	        x += 1
 
-	 	childrenBlend = []
+	#  	childrenBlend = []
 
-	 	j = 0
-	 	t = time.time()
-	 	for process in range(NUM_PROCESSES):
-	 	    pid = os.fork() 
+	#  	j = 0
+	#  	t = time.time()
+	#  	for process in range(NUM_PROCESSES):
+	#  	    pid = os.fork() 
 
-	 	    if pid:
-	 	        childrenBlend.append(pid)
+	#  	    if pid:
+	#  	        childrenBlend.append(pid)
 
-	 	    else:
-	 	        os.execlp('python', 'python', 'analise_operadores.py', "../../Dropbox/operadores-cf0?/CF0" + str(w+1) + "-cxUniform(selWorst, mutShuffleIndexes).txt", str(2), str(11),str(27), str(k)) # overlay program
-	 	        assert False, 'error starting program'   
-	 	        os._exit(0)
-	 	    j += 1
-	 	for i, childBlend in enumerate(childrenBlend):
-	 	    os.waitpid(childBlend, 0)
+	#  	    else:
+	#  	        os.execlp('python', 'python', 'analise_operadores.py', "../../Dropbox/operadores-cf0?/CF0" + str(w+1) + "-cxUniform(selWorst, mutShuffleIndexes).txt", str(2), str(11),str(27), str(k)) # overlay program
+	#  	        assert False, 'error starting program'   
+	#  	        os._exit(0)
+	#  	    j += 1
+	#  	for i, childBlend in enumerate(childrenBlend):
+	#  	    os.waitpid(childBlend, 0)
+
+
+	# for i in range(2):
+	#  	NUM_PROCESSES = 25
+	#  	def timeConsumingFunction():
+	#  	    x = 1
+	#  	    for n in xrange(1000000):
+	#  	        x += 1
+
+	#  	childrenBlend = []
+
+	#  	j = 0
+	#  	t = time.time()
+	#  	for process in range(NUM_PROCESSES):
+	#  	    pid = os.fork() 
+
+	#  	    if pid:
+	#  	        childrenBlend.append(pid)
+
+	#  	    else:
+	#  	        os.execlp('python', 'python', 'analise_operadores.py', "../../Dropbox/operadores-cf0?/CF0" + str(w+1) + "-cxBlend(selWorst, mutFlmutShuffleIndexesipBit).txt", str(3), str(11),str(27), str(k)) # overlay program
+	#  	        assert False, 'error starting program'   
+	#  	        os._exit(0)
+	#  	    j += 1
+	#  	for i, childBlend in enumerate(childrenBlend):
+	#  	    os.waitpid(childBlend, 0)
 
 
 	for i in range(2):
@@ -91,45 +117,19 @@ for w in range(8):
 
 	 	j = 0
 	 	t = time.time()
-	 	for process in range(NUM_PROCESSES):
-	 	    pid = os.fork() 
+		for process in range(NUM_PROCESSES):
+		    pid = os.fork() 
 
-	 	    if pid:
-	 	        childrenBlend.append(pid)
+		    if pid:
+		        childrenBlend.append(pid)
 
-	 	    else:
-	 	        os.execlp('python', 'python', 'analise_operadores.py', "../../Dropbox/operadores-cf0?/CF0" + str(w+1) + "-cxBlend(selWorst, mutFlmutShuffleIndexesipBit).txt", str(3), str(11),str(27), str(k)) # overlay program
-	 	        assert False, 'error starting program'   
-	 	        os._exit(0)
-	 	    j += 1
-	 	for i, childBlend in enumerate(childrenBlend):
-	 	    os.waitpid(childBlend, 0)
-
-
-	#  for i in range(2):
-	#  	NUM_PROCESSES = 25
-	#  	def timeConsumingFunction():
-	#  	    x = 1
-	#  	    for n in xrange(1000000):
-	#  	        x += 1
-
-	#  	childrenBlend = []
-
-	#  	j = 0
-	#  	t = time.time()
-	# 	for process in range(NUM_PROCESSES):
-	# 	    pid = os.fork() 
-
-	# 	    if pid:
-	# 	        childrenBlend.append(pid)
-
-	# 	    else:
-	# 	        os.execlp('python', 'python', 'analise_operadores.py', "../../Dropbox/operadores-cf0?/CF0" + str(w+1) + "-cxSimulatedBinary(selWorst, mutShuffleIndexes).txt", str(4), str(11),str(27), str(k)) # overlay program
-	# 	        assert False, 'error starting program'   
-	# 	        os._exit(0)
-	# 	    j += 1
-	# 	for i, childBlend in enumerate(childrenBlend):
-	# 	    os.waitpid(childBlend, 0)
+		    else:
+		        os.execlp('python', 'python', 'analise_operadores.py', "../../Dropbox/operadores-cf0?/CF0" + str(w+1) + "-cxSimulatedBinary(selWorst, mutShuffleIndexes).txt", str(4), str(11),str(27), str(k)) # overlay program
+		        assert False, 'error starting program'   
+		        os._exit(0)
+		    j += 1
+		for i, childBlend in enumerate(childrenBlend):
+		    os.waitpid(childBlend, 0)
 
 
 	# for i in range(2):
