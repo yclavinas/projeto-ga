8a8dd1441cc8eba65e766d92ea6d1d2a391e022c
lara05 <lara05@lara05pc.(none)>
2014-04-10 21:07:06 -0300

    2 maq

--- testes_cx_deap/100_exec.py
+++ testes_cx_deap/100_exec.py
@@ -3,107 +3,107 @@ for w in range(8):
 	k = 21 + w
 	print k
 
-	for i in range(2):
-		NUM_PROCESSES = 25
-		def timeConsumingFunction():
-		    x = 1
-		    for n in xrange(1000000):
-		        x += 1
+	# for i in range(2):
+	# 	NUM_PROCESSES = 25
+	# 	def timeConsumingFunction():
+	# 	    x = 1
+	# 	    for n in xrange(1000000):
+	# 	        x += 1
 
-		childrenBlend = []
+	# 	childrenBlend = []
 
-		j = 0
-		t = time.time()
-		for process in range(NUM_PROCESSES):
-		    pid = os.fork() 
+	# 	j = 0
+	# 	t = time.time()
+	# 	for process in range(NUM_PROCESSES):
+	# 	    pid = os.fork() 
 
-		    if pid:
-		        childrenBlend.append(pid)
+	# 	    if pid:
+	# 	        childrenBlend.append(pid)
 
-		    else:
-		        os.execlp('python', 'python', 'analise_operadores.py',"../../Dropbox/operadores-cf0?/CF0" + str(w+1) + "-cxOnePoint(selWorst, mutShuffleIndexes).txt", str(0), str(11),str(27), str(k)) # overlay program
-		        assert False, 'error starting program'   
-		        os._exit(0)
-		    j += 1
-		for i, childBlend in enumerate(childrenBlend):
-		    os.waitpid(childBlend, 0)
-
-	for i in range(2):
-		NUM_PROCESSES = 25
-		def timeConsumingFunction():
-		    x = 1
-		    for n in xrange(1000000):
-		        x += 1
-
-		childrenBlend = []
-
-		j = 0
-		t = time.time()
-		for process in range(NUM_PROCESSES):
-		    pid = os.fork() 
-
-		    if pid:
-		        childrenBlend.append(pid)
-
-		    else:
-		        os.execlp('python', 'python', 'analise_operadores.py', "../../Dropbox/operadores-cf0?/CF0" + str(w+1) + "-cxTwoPoints(selWorst, mutShuffleIndexes).txt", str(1), str(11),str(27), str(k)) # overlay program
-		        assert False, 'error starting program'   
-		        os._exit(0)
-		    j += 1
-		for i, childBlend in enumerate(childrenBlend):
-		    os.waitpid(childBlend, 0)
-
+	# 	    else:
+	# 	        os.execlp('python', 'python', 'analise_operadores.py',"../../Dropbox/operadores-cf0?/CF0" + str(w+1) + "-cxOnePoint(selWorst, mutShuffleIndexes).txt", str(0), str(11),str(27), str(k)) # overlay program
+	# 	        assert False, 'error starting program'   
+	# 	        os._exit(0)
+	# 	    j += 1
+	# 	for i, childBlend in enumerate(childrenBlend):
+	# 	    os.waitpid(childBlend, 0)
 
 	# for i in range(2):
 	# 	NUM_PROCESSES = 25
-	#  	def timeConsumingFunction():
-	#  	    x = 1
-	#  	    for n in xrange(1000000):
-	#  	        x += 1
+	# 	def timeConsumingFunction():
+	# 	    x = 1
+	# 	    for n in xrange(1000000):
+	# 	        x += 1
 
-	#  	childrenBlend = []
+	# 	childrenBlend = []
 
-	#  	j = 0
-	#  	t = time.time()
-	#  	for process in range(NUM_PROCESSES):
-	#  	    pid = os.fork() 
+	# 	j = 0
+	# 	t = time.time()
+	# 	for process in range(NUM_PROCESSES):
+	# 	    pid = os.fork() 
 
-	#  	    if pid:
-	#  	        childrenBlend.append(pid)
+	# 	    if pid:
+	# 	        childrenBlend.append(pid)
 
-	#  	    else:
-	#  	        os.execlp('python', 'python', 'analise_operadores.py', "../../Dropbox/operadores-cf0?/CF0" + str(w+1) + "-cxUniform(selWorst, mutShuffleIndexes).txt", str(2), str(11),str(27), str(k)) # overlay program
-	#  	        assert False, 'error starting program'   
-	#  	        os._exit(0)
-	#  	    j += 1
-	#  	for i, childBlend in enumerate(childrenBlend):
-	#  	    os.waitpid(childBlend, 0)
+	# 	    else:
+	# 	        os.execlp('python', 'python', 'analise_operadores.py', "../../Dropbox/operadores-cf0?/CF0" + str(w+1) + "-cxTwoPoints(selWorst, mutShuffleIndexes).txt", str(1), str(11),str(27), str(k)) # overlay program
+	# 	        assert False, 'error starting program'   
+	# 	        os._exit(0)
+	# 	    j += 1
+	# 	for i, childBlend in enumerate(childrenBlend):
+	# 	    os.waitpid(childBlend, 0)
 
 
-	#  for i in range(2):
-	#  	NUM_PROCESSES = 25
-	#  	def timeConsumingFunction():
-	#  	    x = 1
-	#  	    for n in xrange(1000000):
-	#  	        x += 1
+	for i in range(2):
+		NUM_PROCESSES = 25
+	 	def timeConsumingFunction():
+	 	    x = 1
+	 	    for n in xrange(1000000):
+	 	        x += 1
 
-	#  	childrenBlend = []
+	 	childrenBlend = []
 
-	#  	j = 0
-	#  	t = time.time()
-	#  	for process in range(NUM_PROCESSES):
-	#  	    pid = os.fork() 
+	 	j = 0
+	 	t = time.time()
+	 	for process in range(NUM_PROCESSES):
+	 	    pid = os.fork() 
 
-	#  	    if pid:
-	#  	        childrenBlend.append(pid)
+	 	    if pid:
+	 	        childrenBlend.append(pid)
 
-	#  	    else:
-	#  	        os.execlp('python', 'python', 'analise_operadores.py', "../../Dropbox/operadores-cf0?/CF0" + str(w+1) + "-cxBlend(selWorst, mutFlmutShuffleIndexesipBit).txt", str(3), str(11),str(27), str(k)) # overlay program
-	#  	        assert False, 'error starting program'   
-	#  	        os._exit(0)
-	#  	    j += 1
-	#  	for i, childBlend in enumerate(childrenBlend):
-	#  	    os.waitpid(childBlend, 0)
+	 	    else:
+	 	        os.execlp('python', 'python', 'analise_operadores.py', "../../Dropbox/operadores-cf0?/CF0" + str(w+1) + "-cxUniform(selWorst, mutShuffleIndexes).txt", str(2), str(11),str(27), str(k)) # overlay program
+	 	        assert False, 'error starting program'   
+	 	        os._exit(0)
+	 	    j += 1
+	 	for i, childBlend in enumerate(childrenBlend):
+	 	    os.waitpid(childBlend, 0)
+
+
+	for i in range(2):
+	 	NUM_PROCESSES = 25
+	 	def timeConsumingFunction():
+	 	    x = 1
+	 	    for n in xrange(1000000):
+	 	        x += 1
+
+	 	childrenBlend = []
+
+	 	j = 0
+	 	t = time.time()
+	 	for process in range(NUM_PROCESSES):
+	 	    pid = os.fork() 
+
+	 	    if pid:
+	 	        childrenBlend.append(pid)
+
+	 	    else:
+	 	        os.execlp('python', 'python', 'analise_operadores.py', "../../Dropbox/operadores-cf0?/CF0" + str(w+1) + "-cxBlend(selWorst, mutFlmutShuffleIndexesipBit).txt", str(3), str(11),str(27), str(k)) # overlay program
+	 	        assert False, 'error starting program'   
+	 	        os._exit(0)
+	 	    j += 1
+	 	for i, childBlend in enumerate(childrenBlend):
+	 	    os.waitpid(childBlend, 0)
 
 
 	#  for i in range(2):
