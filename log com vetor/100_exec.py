4daa3a52b0ebf5a6992e8dfae1cd9886367a6ef5
lara04 <lara04@lara04pc.(none)>
2014-05-13 20:26:13 -0300

    3

--- log com vetor/100_exec.py
+++ log com vetor/100_exec.py
@@ -46,51 +46,51 @@ for w in range(1):
 	# 	    os.waitpid(childBlend, 0)
 
 
-	# for i in range(2):
-	# 	NUM_PROCESSES = 25
-	#  	childrenBlend = []
+	for i in range(2):
+		NUM_PROCESSES = 25
+	 	childrenBlend = []
 
-	#  	j = 0
-	#  	t = time.time()
-	#  	for process in range(NUM_PROCESSES):
-	#  	    pid = os.fork() 
+	 	j = 0
+	 	t = time.time()
+	 	for process in range(NUM_PROCESSES):
+	 	    pid = os.fork() 
 
-	#  	    if pid:
-	#  	        childrenBlend.append(pid)
+	 	    if pid:
+	 	        childrenBlend.append(pid)
 
-	#  	    else:
-	#  	        os.execlp('python', 'python', 'simple_GA.py', "../../Dropbox/operadores-?/" + str(w+1) + "-cxUniform(selWorst, mutShuffleIndexes).txt", str(2), str(11),str(27), str(k)) # overlay program
-	#  	        assert False, 'error starting program'   
-	#  	        os._exit(0)
-	#  	    j += 1
-	#  	for i, childBlend in enumerate(childrenBlend):
-	#  	    os.waitpid(childBlend, 0)
+	 	    else:
+	 	        os.execlp('python', 'python', 'simple_GA.py', "../../Dropbox/operadores-?/" + str(w+1) + "-cxUniform(selWorst, mutShuffleIndexes).txt", str(2), str(11),str(27), str(k)) # overlay program
+	 	        assert False, 'error starting program'   
+	 	        os._exit(0)
+	 	    j += 1
+	 	for i, childBlend in enumerate(childrenBlend):
+	 	    os.waitpid(childBlend, 0)
 
 
-	# for i in range(2):
-	#  	NUM_PROCESSES = 25
-	#  	def timeConsumingFunction():
-	#  	    x = 1
-	#  	    for n in xrange(1000000):
-	#  	        x += 1
+	for i in range(2):
+	 	NUM_PROCESSES = 25
+	 	def timeConsumingFunction():
+	 	    x = 1
+	 	    for n in xrange(1000000):
+	 	        x += 1
 
-	#  	childrenBlend = []
+	 	childrenBlend = []
 
-	#  	j = 0
-	#  	t = time.time()
-	#  	for process in range(NUM_PROCESSES):
-	#  	    pid = os.fork() 
+	 	j = 0
+	 	t = time.time()
+	 	for process in range(NUM_PROCESSES):
+	 	    pid = os.fork() 
 
-	#  	    if pid:
-	#  	        childrenBlend.append(pid)
+	 	    if pid:
+	 	        childrenBlend.append(pid)
 
-	#  	    else:
-	#  	        os.execlp('python', 'python', 'simple_GA.py', "../../Dropbox/operadores-?/" + str(w+1) + "-cxBlend(selWorst, mutFlmutShuffleIndexesipBit).txt", str(3), str(11),str(27), str(k)) # overlay program
-	#  	        assert False, 'error starting program'   
-	#  	        os._exit(0)
-	#  	    j += 1
-	#  	for i, childBlend in enumerate(childrenBlend):
-	#  	    os.waitpid(childBlend, 0)
+	 	    else:
+	 	        os.execlp('python', 'python', 'simple_GA.py', "../../Dropbox/operadores-?/" + str(w+1) + "-cxBlend(selWorst, mutFlmutShuffleIndexesipBit).txt", str(3), str(11),str(27), str(k)) # overlay program
+	 	        assert False, 'error starting program'   
+	 	        os._exit(0)
+	 	    j += 1
+	 	for i, childBlend in enumerate(childrenBlend):
+	 	    os.waitpid(childBlend, 0)
 
 
 	# # # for i in range(2):
@@ -145,56 +145,56 @@ for w in range(1):
 	# # # 	    os.waitpid(childBlend, 0)
 
 
-	for i in range(2):
-		NUM_PROCESSES = 25
-		def timeConsumingFunction():
-		    x = 1
-		    for n in xrange(1000000):
-		        x += 1
+	# for i in range(2):
+	# 	NUM_PROCESSES = 25
+	# 	def timeConsumingFunction():
+	# 	    x = 1
+	# 	    for n in xrange(1000000):
+	# 	        x += 1
 
-		childrenBlend = []
+	# 	childrenBlend = []
 
-		j = 0
-		t = time.time()
-		for process in range(NUM_PROCESSES):
-		    pid = os.fork() 
+	# 	j = 0
+	# 	t = time.time()
+	# 	for process in range(NUM_PROCESSES):
+	# 	    pid = os.fork() 
 
-		    if pid:
-		        childrenBlend.append(pid)
+	# 	    if pid:
+	# 	        childrenBlend.append(pid)
 
-		    else:
-		        os.execlp('python', 'python', 'simple_GA.py', "../../Dropbox/operadores-?/" + str(w+1) + "-mutFlipBit(selWorst, cxOnePoint).txt", str(0), str(10),str(27), str(k)) # overlay program
-		        assert False, 'error starting program'   
-		        os._exit(0)
-		    j += 1
-		for i, childBlend in enumerate(childrenBlend):
-		    os.waitpid(childBlend, 0)
+	# 	    else:
+	# 	        os.execlp('python', 'python', 'simple_GA.py', "../../Dropbox/operadores-?/" + str(w+1) + "-mutFlipBit(selWorst, cxOnePoint).txt", str(0), str(10),str(27), str(k)) # overlay program
+	# 	        assert False, 'error starting program'   
+	# 	        os._exit(0)
+	# 	    j += 1
+	# 	for i, childBlend in enumerate(childrenBlend):
+	# 	    os.waitpid(childBlend, 0)
 
 
-	for i in range(2):
-		NUM_PROCESSES = 25
-		def timeConsumingFunction():
-		    x = 1
-		    for n in xrange(1000000):
-		        x += 1
+	# for i in range(2):
+	# 	NUM_PROCESSES = 25
+	# 	def timeConsumingFunction():
+	# 	    x = 1
+	# 	    for n in xrange(1000000):
+	# 	        x += 1
 
-		childrenBlend = []
+	# 	childrenBlend = []
 
-		j = 0
-		t = time.time()
-		for process in range(NUM_PROCESSES):
-		    pid = os.fork() 
+	# 	j = 0
+	# 	t = time.time()
+	# 	for process in range(NUM_PROCESSES):
+	# 	    pid = os.fork() 
 
-		    if pid:
-		        childrenBlend.append(pid)
+	# 	    if pid:
+	# 	        childrenBlend.append(pid)
 
-		    else:
-		        os.execlp('python', 'python', 'simple_GA.py', "../../Dropbox/operadores-?/" + str(w+1) + "-mutPolynomialBounded(selWorst, cxOnePoint).txt", str(0), str(12),str(27), str(k)) # overlay program
-		        assert False, 'error starting program'   
-		        os._exit(0)
-		    j += 1
-		for i, childBlend in enumerate(childrenBlend):
-		    os.waitpid(childBlend, 0)
+	# 	    else:
+	# 	        os.execlp('python', 'python', 'simple_GA.py', "../../Dropbox/operadores-?/" + str(w+1) + "-mutPolynomialBounded(selWorst, cxOnePoint).txt", str(0), str(12),str(27), str(k)) # overlay program
+	# 	        assert False, 'error starting program'   
+	# 	        os._exit(0)
+	# 	    j += 1
+	# 	for i, childBlend in enumerate(childrenBlend):
+	# 	    os.waitpid(childBlend, 0)
 
 
 	# for i in range(2):
