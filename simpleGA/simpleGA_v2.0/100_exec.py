ef887b78b9f5d66752e1a05d3154a858dfee53d3
lara05 <lara05@lara05pc.(none)>
2014-09-08 19:31:11 -0300

    0

--- simpleGA/simpleGA_v2.0/100_exec.py
+++ simpleGA/simpleGA_v2.0/100_exec.py
@@ -189,10 +189,53 @@ for w in range(1):
 	# 	    os.waitpid(childBlend, 0)
 
 
-	for i in range(50):
-		NUM_PROCESSES = 1
+	# for i in range(50):
+	# 	NUM_PROCESSES = 1
 		
 
+	# 	childrenBlend = []
+
+	# 	j = 0
+	# 	t = time.time()
+	# 	for process in range(NUM_PROCESSES):
+	# 	    pid = os.fork() 
+
+	# 	    if pid:
+	# 	        childrenBlend.append(pid)
+
+	# 	    else:
+	# 	        os.execlp('python', 'python', 'simpleGA_V2.0.py', "../../../Dropbox/operadores-?/" + str(w+1) + "-selTournament(cxOnePoint, mutShuffleIndexes).txt", str(0), str(11),str(23), str(k)) # overlay program
+	# 	        assert False, 'error starting program'   
+	# 	        os._exit(0)
+	# 	    j += 1
+	# 	for i, childBlend in enumerate(childrenBlend):
+	# 	    os.waitpid(childBlend, 0)
+
+
+	# for i in range(50):
+	# 	NUM_PROCESSES = 1
+
+	# 	childrenBlend = []
+
+	# 	j = 0
+	# 	t = time.time()
+	# 	for process in range(NUM_PROCESSES):
+	# 	    pid = os.fork() 
+
+	# 	    if pid:
+	# 	        childrenBlend.append(pid)
+
+	# 	    else:
+	# 	        os.execlp('python', 'python', 'simpleGA_V2.0.py', "../../../Dropbox/operadores-?/" + str(w+1) + "-selRoulette(cxOnePoint, mutShuffleIndexes).txt", str(0), str(11),str(24), str(k)) # overlay program
+	# 	        assert False, 'error starting program'   
+	# 	        os._exit(0)
+	# 	    j += 1
+	# 	for i, childBlend in enumerate(childrenBlend):
+	# 	    os.waitpid(childBlend, 0)
+
+
+	for i in range(50):
+		NUM_PROCESSES = 1
 		childrenBlend = []
 
 		j = 0
@@ -204,7 +247,7 @@ for w in range(1):
 		        childrenBlend.append(pid)
 
 		    else:
-		        os.execlp('python', 'python', 'simpleGA_V2.0.py', "../../../Dropbox/operadores-?/" + str(w+1) + "-selTournament(cxOnePoint, mutShuffleIndexes).txt", str(0), str(11),str(23), str(k)) # overlay program
+		        os.execlp('python', 'python', 'simpleGA_V2.0.py', "../../../Dropbox/operadores-?/" + str(w+1) + "-selRandom(cxOnePoint, mutShuffleIndexes).txt", str(0), str(11),str(25), str(k)) # overlay program
 		        assert False, 'error starting program'   
 		        os._exit(0)
 		    j += 1
@@ -214,7 +257,6 @@ for w in range(1):
 
 	for i in range(50):
 		NUM_PROCESSES = 1
-
 		childrenBlend = []
 
 		j = 0
@@ -226,51 +268,9 @@ for w in range(1):
 		        childrenBlend.append(pid)
 
 		    else:
-		        os.execlp('python', 'python', 'simpleGA_V2.0.py', "../../../Dropbox/operadores-?/" + str(w+1) + "-selRoulette(cxOnePoint, mutShuffleIndexes).txt", str(0), str(11),str(24), str(k)) # overlay program
+		        os.execlp('python', 'python', 'simpleGA_V2.0.py', "../../../Dropbox/operadores-?/" + str(w+1) + "-selWorst(cxOnePoint, mutShuffleIndexes).txt", str(0), str(11),str(27), str(k))# overlay program
 		        assert False, 'error starting program'   
 		        os._exit(0)
 		    j += 1
 		for i, childBlend in enumerate(childrenBlend):
 		    os.waitpid(childBlend, 0)
-
-
-	# for i in range(50):
-
-	# 	childrenBlend = []
-
-	# 	j = 0
-	# 	t = time.time()
-	# 	for process in range(NUM_PROCESSES):
-	# 	    pid = os.fork() 
-
-	# 	    if pid:
-	# 	        childrenBlend.append(pid)
-
-	# 	    else:
-	# 	        os.execlp('python', 'python', 'simpleGA_V2.0.py', "../../../Dropbox/operadores-?/" + str(w+1) + "-selRandom(cxOnePoint, mutShuffleIndexes).txt", str(0), str(11),str(25), str(k)) # overlay program
-	# 	        assert False, 'error starting program'   
-	# 	        os._exit(0)
-	# 	    j += 1
-	# 	for i, childBlend in enumerate(childrenBlend):
-	# 	    os.waitpid(childBlend, 0)
-
-
-	# for i in range(50):
-
-	# 	childrenBlend = []
-
-	# 	j = 0
-	# 	t = time.time()
-	# 	for process in range(NUM_PROCESSES):
-	# 	    pid = os.fork() 
-
-	# 	    if pid:
-	# 	        childrenBlend.append(pid)
-
-	# 	    else:
-	# 	        os.execlp('python', 'python', 'simpleGA_V2.0.py', "../../../Dropbox/operadores-?/" + str(w+1) + "-selWorst(cxOnePoint, mutShuffleIndexes).txt", str(0), str(11),str(27), str(k))# overlay program
-	# 	        assert False, 'error starting program'   
-	# 	        os._exit(0)
-	# 	    j += 1
-	# 	for i, childBlend in enumerate(childrenBlend):
-	# 	    os.waitpid(childBlend, 0)
